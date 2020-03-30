32b14022189cabd337fd2837a41a52df132371d6
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-03-30 11:13:49 +0300

    Sorting algorithm added

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,2 +1,17 @@
-puts 'Hello World!'
-puts 'We are Ikraam and Kubilay!'
+def bubble_sort(array)
+  print "IN : #{array}\n"
+  iteration = array.length - 1
+  (0...iteration).each do |_round|  # 1 ROUND ITERATION
+    swapped = false
+    (0...iteration).each do |i|   # 2 ITEM ITERATION
+        if array[i] > array[i + 1]
+            array[i], array[i + 1] = array[i + 1], array[i]        
+            swapped = true
+        end
+    end
+    break unless swapped
+  end
+  print "OUT: #{array}\n"
+end
+
+bubble_sort([4,3,78,2,0,2])
