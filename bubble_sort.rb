626fe50e5d802a69417b81f6f0bee967f6a20dab
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-03-30 11:26:47 +0300

    Bubble_sort_by function added

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,37 @@
 def bubble_sort(array)
   print "IN : #{array}\n"
   iteration = array.length - 1
-  (0...iteration).each do |_round|
+  (0...iteration).each do |round|
     swapped = false
-    (0...(iteration - _round)).each do |i|
-        if array[i] > array[i + 1]
-            array[i], array[i + 1] = array[i + 1], array[i]        
-            swapped = true
-        end
+    (0...(iteration - round)).each do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
     end
     break unless swapped
   end
   print "OUT: #{array}\n"
 end
 
-bubble_sort([4,3,78,2,0,2])
+bubble_sort([4, 3, 78, 2, 0, 2])
+
+def bubble_sort_by(array)
+    print "IN : #{array}\n"
+    iteration = array.length - 1
+    (0...iteration).each do |round|
+      swapped = false
+      (0...(iteration - round)).each do |i|
+        first = array[i]
+        second = array[i + 1]
+        if yield(first, second).positive?
+          array[i], array[i + 1] = array[i + 1], array[i]
+          swapped = true
+        end
+      end
+      break unless swapped
+    end
+    print "OUT: #{array}\n"
+    puts '--'
+    puts 'end'
+end
