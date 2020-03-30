fd98aeb9b0c25523b3bf4ff2a5e4f94facf2543b
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-03-30 11:30:13 +0300

    Rubocop errors fixed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -17,21 +17,23 @@ end
 bubble_sort([4, 3, 78, 2, 0, 2])
 
 def bubble_sort_by(array)
-    print "IN : #{array}\n"
-    iteration = array.length - 1
-    (0...iteration).each do |round|
-      swapped = false
-      (0...(iteration - round)).each do |i|
-        first = array[i]
-        second = array[i + 1]
-        if yield(first, second).positive?
-          array[i], array[i + 1] = array[i + 1], array[i]
-          swapped = true
-        end
+  print "IN : #{array}\n"
+  iteration = array.length - 1
+  (0...iteration).each do |round|
+    swapped = false
+    (0...(iteration - round)).each do |i|
+      first = array[i]
+      second = array[i + 1]
+      if yield(first, second).positive?
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
       end
-      break unless swapped
     end
-    print "OUT: #{array}\n"
-    puts '--'
-    puts 'end'
+    break unless swapped
+  end
+  print "OUT: #{array}\n"
+end
+
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
 end
